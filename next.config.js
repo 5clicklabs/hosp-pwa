02d8ba9f41935b3a128423c63ea89b338a5bdc6a
briha <hello@briha.xyz>
2024-06-29 19:38:04 +0530

    trial

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+};
+
 const withPWA = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withPWA({
-  reactStrictMode: true,
-});
+module.exports = withPWA(nextConfig);
