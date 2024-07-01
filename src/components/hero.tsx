c86e99acf95637615e0191709f7cc90ba58a1e16
briha <hello@briha.xyz>
2024-07-01 22:09:52 +0530

    added toggle and animation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,9 @@
 import useFrequentlyAskedOperations from "@/hooks/frequent-ops";
 import { Flex, Text } from "@chakra-ui/react";
 import Card from "./core/frequent-card";
+import { Switch } from "./ui/switch";
+import React from "react";
+import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
@@ -13,28 +16,46 @@ export default function Hero() {
 }
 
 function MostFrequentlyAsked() {
+  const [isChecked, setIsChecked] = React.useState(false);
   const { options } = useFrequentlyAskedOperations();
+
   return (
     <div className="my-5">
-      <Text variant="subheading">Most Frequently Asked</Text>
-
-      <Flex
-        style={{ overflowX: "scroll" }}
-        align="center"
-        py={4}
-        justify={{ base: "flex-start", lg: "space-evenly" }}
-      >
-        {options.map((item, index) => (
-          <Card
-            color={item.color}
-            logo={item.logo}
-            heading={item.heading}
-            subheading={item.subheading}
-            directive={item.directive}
-            key={index}
-          />
-        ))}
+      <Flex justify="space-between" align="center">
+        <Text variant="subheading">Most Frequently Asked</Text>
+        <Switch
+          checked={isChecked}
+          onCheckedChange={() => setIsChecked(!isChecked)}
+        />
       </Flex>
+
+      <motion.div
+        initial={{ height: 0, opacity: 0 }}
+        animate={{
+          height: isChecked ? "auto" : 0,
+          opacity: isChecked ? 1 : 0,
+        }}
+        transition={{ duration: 0.3 }}
+        style={{ overflow: "hidden" }}
+      >
+        <Flex
+          align="center"
+          py={4}
+          justify={{ base: "flex-start", lg: "space-evenly" }}
+          style={{ overflowX: "scroll" }}
+        >
+          {options.map((item, index) => (
+            <Card
+              color={item.color}
+              logo={item.logo}
+              heading={item.heading}
+              subheading={item.subheading}
+              directive={item.directive}
+              key={index}
+            />
+          ))}
+        </Flex>
+      </motion.div>
     </div>
   );
 }
